b7c3938a89336a8b0160bc4d3f34ba82815b3b44
Mhiaghi <miguel.saturno@utec.edu.pe>
2021-01-08 23:32:38 -0500

    first commit

--- codigo.js
+++ codigo.js
@@ -2,13 +2,11 @@ const db = firebase.firestore();
 
 db.collection("dispositivos").doc("Disp01")
     .onSnapshot(function(doc) {
-        console.log("Temperatura: ", doc.data().Temperatura);
         document.getElementById("temperatura_value").innerHTML = doc.data().Temperatura + "°C";
     });
 
 db.collection("dispositivos").doc("Disp01")
 .onSnapshot(function(doc) {
-    console.log("Distancia: ", doc.data().Distancia);
     document.getElementById("distancia_value").innerHTML = doc.data().Distancia + " m";
 });
 
@@ -22,7 +20,6 @@ db.collection("dispositivos").doc("Disp01")
     {
         var alarma_valor = "Apagado"
     }
-    console.log("Alarma: ", );
     document.getElementById("alarma_value").innerHTML = alarma_valor;
 });
 
